9a60fa9128f694f883b9bb6e2caf8205cac2d8bd
Futsey <shmopls@gmail.com>
2021-09-06 22:23:41 +0500

    4.3.6. Math.abs [#373616]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,7 +1,13 @@
 package ru.job4j;
 
 public class Main {
+
+    public static boolean check(int num) {
+        return num < 0 || num % 2 != 0;
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println(check(2));
     }
 }
