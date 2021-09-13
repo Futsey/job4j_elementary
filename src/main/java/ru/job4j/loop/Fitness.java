67e75b4cf01d520061cbae8f0be7030c7c850750
Futsey <shmopls@gmail.com>
2021-09-13 13:07:17 +0500

    5.4. Протеиновая диета [#156313 #202486]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -4,9 +4,9 @@ public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
         while (ivan <= nik) {
-            ivan * =3;
-            nik * = 2;
-            month + =1;
+            ivan = ivan * 3;
+            nik = nik * 2;
+            month = month + 1;
         }
         return month;
     }
