da7e1597046537196ee340165c236bc3253d9fb0
Futsey <shmopls@gmail.com>
2021-09-16 14:42:12 +0500

    6.0.2. Заполнение массива. [#226300 #203906]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -10,12 +10,12 @@ public class ArrayDefinition {
             System.out.println("Размер массива pieces: " + pieces.length);
         String[] names = new String[4];
             names[0] = "Andrey";
-        System.out.println("Объект 1 ваше имя: " + names[1]);
+        System.out.println("Объект 1 ваше имя: " + names[0]);
             names[1] = "Tommy";
-        System.out.println("Объект 2 ваше имя: " + names[2]);
+        System.out.println("Объект 2 ваше имя: " + names[1]);
             names[2] = "Gaby";
-        System.out.println("Объект 3 ваше имя: " + names[3]);
+        System.out.println("Объект 3 ваше имя: " + names[2]);
             names[3] = "Evlampiy";
-        System.out.println("Объект 4 ваше имя: " + names[4]);
+        System.out.println("Объект 4 ваше имя: " + names[3]);
     }
 }
