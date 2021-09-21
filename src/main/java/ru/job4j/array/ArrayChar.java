d550a99911abc65dcc61b64cb9caf0a2e9025ff8
Futsey <shmopls@gmail.com>
2021-09-21 20:57:30 +0500

    6.4. Слово начинается с ... [#41585]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        for (int i = 0; i < pref.length - 1; i++) {
+            if (pref[i] != word[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
+
+
+
