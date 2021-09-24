533c1773a3a0c6811112a779b331e91a3b0ddaae
Futsey <shmopls@gmail.com>
2021-09-24 15:34:55 +0500

    6.4.1.1. Обход массива с последнего элемента [#361619 #206884]

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -5,11 +5,11 @@ public class SortSelected {
     public static int[] sort(int[] data) {
         int result;
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, 0, data.length);
-            int index = FindLoop.indexOfSection(data, min, 0, data.length);
-            int temp = data[min];
-            data[min] = data[index];
-            data[index] = temp;
+            int min = MinDiapason.findMin(data, i, data.length);
+            int index = FindLoop.indexOfSection(data, min, i, data.length);
+            int temp = min;
+            min = index;
+            index = temp;
         }
         return data;
     }
