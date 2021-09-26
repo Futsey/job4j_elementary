f2aea4538dcae01a069183c235291431075fe7e4
Futsey <shmopls@gmail.com>
2021-09-26 10:26:25 +0500

    6.5.3. Сортировка выборкой. [#156320]

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -5,11 +5,15 @@ public class SortSelected {
     public static int[] sort(int[] data) {
         int result;
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, i, data.length);
-            int index = FindLoop.indexOf(data, min, i, data.length);
-            int temp = min;
-            min = index;
-            index = temp;
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            System.out.println("Method MinDiapason.findMin: Получили минимальное значение элемента: " + min);
+            int index = FindLoop.indexOf(data, min, i, data.length - 1);
+            System.out.println("Method FindLoop.indexOf: Получили значение индекса элемента с минимальным значением: " + index);
+
+            int temp = data[i];
+            data[i] = data[index];
+            data[index] = temp;
+            System.out.println("Method sort: Заменили элемент : " + temp + " на наименьший: " + data[i]);
         }
         return data;
     }
--- test/java/ru/job4j/array/SortSelectedTest.java
+++ test/java/ru/job4j/array/SortSelectedTest.java
@@ -11,4 +11,20 @@ public class SortSelectedTest {
         int[] expected = new int[] {1, 2, 3, 4, 5};
         Assert.assertArrayEquals(expected, result);
     }
+
+    @Test
+    public void whenSort17and24and42() {
+        int[] data = new int[] {24, 42, 17};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {17, 24, 42};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSort6and12and36and77and548() {
+        int[] data = new int[] {36, 12, 77, 548, 6};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {6, 12, 36, 77, 548};
+        Assert.assertArrayEquals(expected, result);
+    }
 }
