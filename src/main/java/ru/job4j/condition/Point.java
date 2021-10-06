49a0186754224059bf829f7052e363461b1be80a
Futsey <shmopls@gmail.com>
2021-10-06 09:28:09 +0500

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,24 +1,41 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
 
     private int x;
     private int y;
+    private int z;
 
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public double distance(Point that) {
-        double rsl = Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
-        return rsl;
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+
+    public void info() {
+        System.out.println("Point[" + this.x + ", " + this.y + ", " +  this.z + "]");
     }
 
     public static void main(String[] args) {
-        Point a = new Point(-1, 4);
-        Point b = new Point(2, 0);
-        double dist = a.distance(b);
+        Point a = new Point(-1, 4, 6);
+        Point b = new Point(2, 0, 8);
+        double dist = a.distance3d(b);
         System.out.println(dist);
     }
 }
--- test/java/ru/job4j/condition/PointTest.java
+++ test/java/ru/job4j/condition/PointTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class PointTest {
     @Test
-    public void when30to36then6() {
+    public void when30to36Then6() {
         double expected = 6;
         Point a = new Point(3, 0);
         Point b = new Point(3, 6);
@@ -14,7 +14,7 @@ public class PointTest {
     }
 
     @Test
-    public void when30to218then18dot02() {
+    public void when30to218Then18dot02() {
         double expected = 18.02;
         Point a = new Point(3, 0);
         Point b = new Point(2, 18);
@@ -23,7 +23,7 @@ public class PointTest {
     }
 
     @Test
-    public void when56to18then4dot47() {
+    public void when56to18Then4dot47() {
         double expected = 4.47;
         Point a = new Point(5, 6);
         Point b = new Point(1, 8);
@@ -32,11 +32,38 @@ public class PointTest {
     }
 
     @Test
-    public void whenMinus1and4to20then5() {
+    public void whenMinus1and4to20Then5() {
         double expected = 5;
         Point a = new Point(-1, 4);
         Point b = new Point(2, 0);
         double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
+    }
+
+    @Test
+    public void whenMinus1and4and6to2and0and8Then5dot38() {
+        double expected = 5.38;
+        Point a = new Point(-1, 4, 6);
+        Point b = new Point(2, 0, 8);
+        double dist = a.distance3d(b);
+        Assert.assertEquals(expected, dist, 0.01);
+    }
+
+    @Test
+    public void when1and2and3to5and6and7Then6dot92() {
+        double expected = 6.92;
+        Point a = new Point(1, 2, 3);
+        Point b = new Point(5, 6, 7);
+        double dist = a.distance3d(b);
+        Assert.assertEquals(expected, dist, 0.01);
+    }
+
+    @Test
+    public void whenZeroThen0dot0() {
+        double expected = 0.0;
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 0, 0);
+        double dist = a.distance3d(b);
         Assert.assertEquals(expected, dist, 0.01);
     }
 }
