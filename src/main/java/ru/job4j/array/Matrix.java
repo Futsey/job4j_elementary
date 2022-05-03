bc1c39afaee17c888c77bb8f9c2f9fb0450dcc2f
Fut <shmopls@gmail.com>
2022-05-03 07:20:46 +0500

    FileOutputStream

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -1,15 +1,28 @@
 package ru.job4j.array;
 
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.util.Arrays;
+
 public class Matrix {
 
     public static int[][] multiple(int size) {
         int[][] array = new int[size][size];
-
-        for (int row = 0; row < size; row++) {
-            for (int cell = 0; cell < size; cell++) {
-                array[row][cell] = (cell + 1) * (row + 1);
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int row = 0; row < size; row++) {
+                for (int cell = 0; cell < size; cell++) {
+                    array[row][cell] = (cell + 1) * (row + 1);
+                    out.write(Arrays.deepToString(array).getBytes());
+                    out.write(System.lineSeparator().getBytes());
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return array;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(multiple(3)));
     }
 }
